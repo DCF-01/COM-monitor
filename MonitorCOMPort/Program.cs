84441fd7629557f57f7c8b3018b1883c497fbd70
DCF-01 <rkezunovski98@gmail.com>
2022-04-02 12:55:06 +0200

    first

---
+++ MonitorCOMPort/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO.Ports;
+using System.Management;
+
+SerialPort.GetPortNames().Select(x => { Console.WriteLine(x); return x; }).ToList();
+
+string portName = Console.ReadLine();
+SerialPort port = new SerialPort(portName, 9600, Parity.None, 8, StopBits.One);
+ReadFromCOMPort();
+
+void ReadFromCOMPort()
+{
+    Console.WriteLine("Incoming data:");
+    port.DataReceived += new SerialDataReceivedEventHandler(PortDataReceived);
+
+
+    port.Open();
+
+    while (true)
+    {
+
+    }
+}
+
+void PortDataReceived(object sender, SerialDataReceivedEventArgs e)
+{
+    Console.WriteLine(port.ReadExisting());
+}
